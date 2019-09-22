baee5e1e155455f49bb77e29079efa04931bf07a
Vladimir Babenko <babenkovladimirbmd@gmail.com>
2019-09-22 17:40:04 +0300

    update lifecycle version;

--- app/src/main/java/com/babenkovladimir/androidlesson7architecture/mvvm/MvvmActivity.java
+++ app/src/main/java/com/babenkovladimir/androidlesson7architecture/mvvm/MvvmActivity.java
@@ -1,11 +1,13 @@
 package com.babenkovladimir.androidlesson7architecture.mvvm;
 
+import android.app.Application;
 import android.content.Context;
 import android.os.Bundle;
 import androidx.appcompat.app.AlertDialog.Builder;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 import com.babenkovladimir.androidlesson7architecture.R;
 import com.babenkovladimir.androidlesson7architecture.databinding.ActivityMvvmBinding;
@@ -20,7 +22,8 @@ public class MvvmActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
 
     ActivityMvvmBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_mvvm);
-    mModel = ViewModelProviders.of(this).get(MyViewModel.class);
+//    mModel = ViewModelProviders.of(this, factory).get(MyViewModel.class);
+    mModel = new ViewModelProvider(this).get(MyViewModel.class);//ViewModelProviders.of(this, factory).get(MyViewModel.class);
     binding.setViewModel(mModel);
     binding.setLifecycleOwner(this);
     getLifecycle().addObserver(mModel);
